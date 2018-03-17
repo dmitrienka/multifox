d1ffe8444cb3c64e3d92b24116b2e92bf38a001f
Artem Dmitrienko <dmitrienka@gmail.com>
2018-03-17 23:50:36 +0300

    controlable number of processes

--- multifox.sh
+++ multifox.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-for i in `seq 1 24`; do
-    mkdir fox24_out_$i
-    cp $1 fox24_out_$i
-    cd fox24_out_$i
+for i in `seq 1 $1`; do
+    mkdir multifox_out_$i
+    cp $2 multifox_out_$i
+    cd multifox_out_$i
     num=$RANDOM
     let "num %= 3000"
-    /home/dmitrienka/bin/Fox-nogui $1  --silent --randomize -n $num -o $1_new_$num.xml
-    /home/dmitrienka/bin/Fox-nogui $1_new_$num.xml  --silent --randomize -n $2 --nbrun  $3  -o fox24_out_$i.xml  &
+    /home/dmitrienka/bin/Fox-nogui $2  --silent --randomize -n $num -o $2_new_$num.xml
+    /home/dmitrienka/bin/Fox-nogui $2_new_$num.xml  --silent --randomize -n $3 --nbrun  $4  -o multifox_out_$i.xml  &
     cd ..
 done
 wait
