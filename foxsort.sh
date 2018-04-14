8b2d0a35d1379ac26243bd62de1c584f149dbcb8
Artem Dmitrienko <dmitrienka@gmail.com>
2018-04-14 19:14:38 +0300

    fix foxsort

--- foxsort.sh
+++ foxsort.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
- find | grep Cost | sed 's/[.]\/fox24_out_\([0-9]*\)\/OptimizationObj #[0-9\_-]*Run#[0-9]*-Cost-\([0-9.]*\)[.]xml/\2\t&/' | sort -t \t -k 1 -g -r | cut -f2 
+ find | grep Cost | sed 's/[.]\/multifox_out_\([0-9]*\)\/OptimizationObj #[0-9\_-]*Run#[0-9]*-Cost-\([0-9.]*\)[.]xml/\2\t&/' | sort -t \t -k 1 -g -r | cut -f2 
